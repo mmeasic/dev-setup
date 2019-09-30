cc1826e3f317072d638ca61380338286f0e68fde
Mario Measic <mario.measic.gavran@gmail.com>
2019-09-30 16:07:42 +0200

    Latest changes for setting up the environment.

--- pydata.sh
+++ pydata.sh
@@ -34,35 +34,19 @@ echo "" >> $EXTRA_PATH
 echo "" >> $EXTRA_PATH
 echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
 echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+
+# Setup the Python dev path
+DEV_PATH=~/code/dev/python
+mkdir -p DEV_PATH
+echo $DEV_PATH
+echo "" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "# Project home path for virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
+echo "export PROJECT_HOME=~/code/dev/python" >> $EXTRA_PATH
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
-echo "" >> $BASH_PROFILE_PATH
+echo "" >> $EXTRA_PATH
 source $EXTRA_PATH
-
-###############################################################################
-# Python 2 Virtual Enviroment                                                 #
-###############################################################################
-
-echo "------------------------------"
-echo "Setting up py2-data virtual environment."
-
-# Create a Python2 data environment
-mkvirtualenv py2-data
-workon py2-data
-
-# Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
 
 ###############################################################################
 # Python 3 Virtual Enviroment                                                 #
@@ -89,6 +73,17 @@ pip install "ipython[all]"
 pip install bokeh
 pip install Flask
 pip install sqlalchemy
+pip install cookiecutter
+pip install jupyter_contrib_nbextensions
+
+# Setting up the cookiecutter DS template
+COOKIE_GITHUB=https://github.com/mmeasic/cookiecutter-data-science
+echo "" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+echo "# Cookiecutter template location" >> $EXTRA_PATH
+echo "export COOKIE_GITHUB=$COOKIE_GITHUB" >> $EXTRA_PATH
+echo "" >> $EXTRA_PATH
+source $EXTRA_PATH
 
 ###############################################################################
 # Install IPython Profile
@@ -100,6 +95,31 @@ echo "Installing IPython Notebook Default Profile"
 # Add the IPython profile
 mkdir -p ~/.ipython
 cp -r init/profile_default/ ~/.ipython/profile_default
+
+###############################################################################
+# Global Jupyter NBExtensions in ~/.jupyter/nbconfig, valid for all envs
+###############################################################################
+
+# Add the notebook extension style files
+jupyter contrib nbextension install
+
+# Add the tab to the web UI
+pip install jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable
+
+# Enable default extensions
+# AutoPEP8
+jupyter nbextension enable code_prettify/autopep8
+# TOC
+jupyter nbextension enable toc2/main
+# Variable inspector
+jupyter nbextension enable varInspector/main
+# Code folding
+jupyter nbextension enable codefolding/main
+# ExecuteTime
+jupyter nbextension enable execute_time/ExecuteTime
+# GistIt
+jupyter nbextension enable gist_it/main
 
 echo "------------------------------"
 echo "Script completed."
