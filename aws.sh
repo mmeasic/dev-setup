88cd4e64408209dbff0c737e146ab0de2898ac3e
Mario Measic <mario.measic.gavran@gmail.com>
2020-03-06 08:49:06 +0100

    Remove pydata2 reference from aws.

--- aws.sh
+++ aws.sh
@@ -16,32 +16,6 @@ echo "Source virtualenvwrapper from ~/.extra"
 source ~/.extra
 
 ###############################################################################
-# Python 2 Virtual Enviroment                                                 #
-###############################################################################
-
-echo "------------------------------"
-echo "Updating py2-data virtual environment with AWS modules."
-
-# Create a Python2 data environment
-# If this environment already exists from running pydata.sh,
-# it will not be overwritten
-mkvirtualenv py2-data
-workon py2-data
-
-pip install boto
-pip install awscli
-pip install mrjob
-pip install s3cmd
-
-EXTRA_PATH=~/.extra
-echo $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "# Configure aws cli autocomplete, added by aws.sh" >> $EXTRA_PATH
-echo "complete -C '~/.virtualenvs/py2-data/bin/aws_completer' aws" >> $EXTRA_PATH
-source $EXTRA_PATH
-
-###############################################################################
 # Python 3 Virtual Enviroment                                                 #
 ###############################################################################
 
